2ca89cdd400cfb2b008594ebad6f58c9a83f2216
Idriss Deche <idriss@Mac-idriss.local>
2025-12-08 02:34:48 +0100

    update

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$dbname = 'platforme_formations';
+$username = 'root';
+$password = '';
+
+$mysqli = mysqli_connect($host, $username, $password, $dbname);
+
+if(!$mysqli) {
+    echo "connexion échoué";
+}
+?>
---
+++ signup.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+include 'config.php';
+
+if(isset($_POST['submit'])) {
+
+$name = $_POST['name'];
+$prenom = $_POST['prenom'];
+$email = $_POST['email'];
+$telephone = $_POST['telephone'];
+$password = $_POST['password'];
+
+$hash_password = password_hash($password, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO UTILISATEUR (email, mot_de_passe, nom, prenom , telephone) VALUES ('$email', '$hash_password', '$name', '$prenom', '$telephone')";
+
+if(mysqli_query($mysqli, $sql)) {
+    echo 'sign up seccussful!';
+} else {
+    echo 'error' . mysqli_error($mysqli);
+}}
+?>
