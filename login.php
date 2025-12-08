779f6c92513730336060cedd6e8d662ab246ca2d
Idriss Deche <idriss@Mac-idriss.local>
2025-12-08 19:36:50 +0100

    adding login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+include 'config.php';
+
+if(isset($_POST['submit'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = mysqli_query($mysqli, "SELECT * FROM UTILISATEUR WHERE email = '$email'");
+
+    if(mysqli_num_rows($result) == 0){
+        echo "email not found";
+    } else {
+        $row = mysqli_fetch_assoc($result);
+
+    if(password_verify($password, $row['mot_de_passe'])){
+        echo "<h1>hello<h1/>"; } else {
+            echo "wrong password"; }
+        }
+
+
+}
+?>
